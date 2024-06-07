005cd2a7d344c64c98144c4d7c793b81cbd3bf03
200~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~ <louannevillette2000@gmail.com>
2024-06-07 10:23:41 -0400

    Création du fichier "main.py"

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Programme de chiffrement et de déchiffrement utilisant le code de César en Python.
+Ce programme permet de chiffrer et déchiffrer des textes ou des fichiers texte en utilisant une clé spécifiée.
+Il peut également tenter de déchiffrer des textes sans clé en utilisant une analyse fréquentielle des caractères ou
+par essai-erreur en vérifiant le pourcentage de mots appartenant aux mots français les plus fréquents.
+
+Auteurs : Lou-Anne Villette, Thomas Chambeyron
+Date : 22/05/2024
+"""
